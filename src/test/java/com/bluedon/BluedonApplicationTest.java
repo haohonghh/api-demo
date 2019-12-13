6d7547e68f3231d9b8a0bdba6a7517f0df3bea72
haohong <haohong278636029@vip.qq.com>
2019-12-13 14:17:59 +0800

    init

--- src/test/java/com/bluedon/BluedonApplicationTest.java
+++ src/test/java/com/bluedon/BluedonApplicationTest.java
@@ -1,8 +1,14 @@
 package com.bluedon;
 
+import com.alibaba.fastjson.JSON;
+import com.bluedon.common.utils.SpringContextUtils;
+import com.bluedon.modules.student.entity.StudentEntity;
+import com.bluedon.modules.student.service.StudentService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -15,8 +21,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class BluedonApplicationTest {
 
+    @Autowired
+    private SpringContextUtils springContextUtils;
+
+
     @Test
-    public void test(){
-        System.out.println("----");
+    public void springContextUtilsTest(){
+        StudentService studentService = (StudentService) springContextUtils.getBean("studentService");
+        StudentEntity byId = studentService.getById("5ca8a85bdfc9b7e0903bb2f48e07b874");
+        System.out.println(JSON.toJSONString(byId));
     }
 }
