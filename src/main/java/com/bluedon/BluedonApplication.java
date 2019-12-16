f95e785fa83a37b64d94fedb8197fc874a876f0a
haohong <haohong278636029@vip.qq.com>
2019-12-16 11:25:14 +0800

    init

--- src/main/java/com/bluedon/BluedonApplication.java
+++ src/main/java/com/bluedon/BluedonApplication.java
@@ -2,6 +2,8 @@ package com.bluedon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
@@ -12,3 +14,18 @@ public class BluedonApplication {
 	}
 
 }
+
+//外置 tomcat部署方式
+/*@SpringBootApplication
+public class BluedonApplication extends SpringBootServletInitializer {
+	public static void main(String[] args) {
+		Long time=System.currentTimeMillis();
+		SpringApplication.run(BluedonApplication.class);
+		System.out.println("===应用启动耗时："+(System.currentTimeMillis()-time)+"===");
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
+	}
+}*/
